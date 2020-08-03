7ebb6a6cf9dd24491f368ec4ad438d7d530ed2e9
daniel71-n <h38c@protonmail.com>
2020-08-03 17:36:15 +0200

    Added 'next' and 'previous' links to the post_detail template
    The next and previous links only span the articles of a single category,
    not all categories. When the article is the last one in the category,
    the 'next' link disappears, and when the article is the first one in a
    given category, there's no 'previous' link.

--- posts/views.py
+++ posts/views.py
@@ -18,11 +18,35 @@ class PostListView(ListView):
         return context
 
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-    context_object_name = 'post' #if we don't override this, the default would have to be used, which is object, e.g. {{ object.title }}
 
+class PostDetailView(DetailView):
+    template_name='post_detail.html'
+    model = Post
+    context_object_name = 'post' 
+    
+    def get_context_data(self, **kwargs):
+        context=super(PostDetailView, self).get_context_data(**kwargs)
+        
+        all_articles = [i.title for i in self.object.section.post_set.all()]  # make the queryset into a list, so that it can be indexed
+        current_index = all_articles.index(self.object.title)     # querysets aren't lists, but can be converted to one
+
+        # determine whether or not there's a previous article
+        if current_index > 0:   # there's at least one article before this
+            previous_neighbor_title = all_articles[current_index-1]    # get the title at the index before current_index
+            prev_obj = Post.objects.get(title=previous_neighbor_title)   # get the object that has that title
+            context['previous'] = prev_obj    # make this object accessible via a 'previous' context variable
+        else:
+            context['previous'] = False   # there's no 'previous' article, which needs to be reflected on the Web page
+      
+       # determine whether or not there's a next article
+        if len(all_articles) >= current_index+2:     # if there's at least one more article in this category after this one
+            next_neighbor_title = all_articles[current_index+1] 
+            next_obj = Post.objects.get(title=next_neighbor_title)
+            context['next'] = next_obj 
+        else:
+            context['next'] = False
+
+        return context
 
 
 class CategoryView(DetailView):
