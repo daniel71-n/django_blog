6d750e0775f22f31ae0d110048314b03330478c7
daniel71-n <h38c@protonmail.com>
2020-08-17 10:19:38 +0200

    moved javascript and css into separate static files in /staticfiles.
    Took the css and javascript out of the respective html document they
    appeared in and put it all in dedicated files. Specifically: base.css,
    home_ajax.js, and comments_toggle.js.
    
    These files I then moved into the /staticfiles directory that
    settings.py has been updated to look in for its STATICFILES_DIRS variable.

---
+++ staticfiles/comments_toggle.js
@@ -0,0 +1,7 @@
+// Jquery to revel/hide comments at the bottom of the document on buttonclick
+$(document).ready(function(){
+		   $('#comment-toggle').click(function() {
+			   $('.show-hide').toggle("slide");
+		   });
+	   });
+
